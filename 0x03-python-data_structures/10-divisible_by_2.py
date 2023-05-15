d32557573b71618b14ad47c54272c2b0a4db2a73
Ukeremi <ukpono9@gmail.com>
2023-05-15 11:46:44 -0700

    multiple returns

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """find all multiples of 2 in a list"""
+    aggregate = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            aggregate.append(True)
+        else:
+            aggregate.append(False)
+
+            return(aggregate)
