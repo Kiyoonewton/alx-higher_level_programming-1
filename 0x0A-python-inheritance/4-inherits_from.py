5aa82c85692bf44375d473da5bec11ae0e5c5c6a
Ukeremi <ukpono9@gmail.com>
2023-06-12 04:13:09 -0700

    function returns true when object is instance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """
-Check for subclass
+===================================
+module with method inherits_from
+===================================
 """
 
 
 def inherits_from(obj, a_class):
-    """returns true if obj is a subclass of a_class"""
-    return(issubclass(type(obj), a_class))
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
